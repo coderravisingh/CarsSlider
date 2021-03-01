a1a67af560987bb66610389641b3a0f4dc32b8ff
ravisingh <2796.rravi@gmail.com>
2021-03-01 21:22:57 +0530

    card slider user

--- CarsSlider/ViewController.swift
+++ CarsSlider/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import CardSlider
 
-class ViewController: UIViewController {
+
+struct Item: CardSliderItem {
+    var image: UIImage
+    var rating: Int?
+    var title: String
+    var subtitle: String?
+    var description: String?
+    
+
+}
+
+class ViewController: UIViewController, CardSliderDataSource {
+    
+   
+  
+    var data = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      dataAdd()
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        let vc = CardSliderViewController.with(dataSource: self)
+        vc.title = "Slide"
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
     }
 
+
+    
+    func item(for index: Int) -> CardSliderItem {
+        return data[index]
+    }
+    
+    func numberOfItems() -> Int {
+        return data.count
+    }
+    
+    func dataAdd() {
+        data.append(Item(image: UIImage(systemName: "person")!,
+                         rating: nil,
+                         title: "",
+                         subtitle: "",
+                         description: ""))
+        data.append(Item(image: UIImage(systemName: "person")!,
+                         rating: nil,
+                         title: "",
+                         subtitle: "",
+                         description: ""))
+        data.append(Item(image: UIImage(systemName: "person")!,
+                         rating: nil,
+                         title: "",
+                         subtitle: "",
+                         description: ""))
+    }
 
 }
 
